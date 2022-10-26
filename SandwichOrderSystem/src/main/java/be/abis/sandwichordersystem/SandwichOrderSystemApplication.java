e2ae50f9c1087d0febd726608297eb413a155b95
Marcel van Hassel <marcelvanhassel@gmail.com>
2022-10-26 11:11:47 +0200

    initial commit

---
+++ SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/SandwichOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package be.abis.sandwichordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandwichOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandwichOrderSystemApplication.class, args);
+    }
+
+}
---
+++ SandwichOrderSystem/src/test/java/be/abis/sandwichordersystem/SandwichOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package be.abis.sandwichordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandwichOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
