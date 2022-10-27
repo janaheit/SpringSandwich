b33c4e75d5e6dc42ebab551a53c6e590a12b61a5
Jana Heitkemper <janaheitkemper@gmail.com>
2022-10-27 10:13:10 +0200

    findmethods implemented

---
+++ SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/service/SessionServiceImpl.java
@@ -0,0 +1,56 @@
+package be.abis.sandwichordersystem.service;
+
+import be.abis.sandwichordersystem.exception.SessionNotFoundException;
+import be.abis.sandwichordersystem.model.Instructor;
+import be.abis.sandwichordersystem.model.Person;
+import be.abis.sandwichordersystem.model.Session;
+import be.abis.sandwichordersystem.repository.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class SessionServiceImpl implements SessionService {
+
+    @Autowired private SessionRepository sessionRepository;
+
+    public SessionServiceImpl() {
+    }
+
+    @Override
+    public List<Person> findAllPersonsFollowingSession(Session session) {
+        return sessionRepository.findAllPersonsFollowingSession(session);
+    }
+
+    @Override
+    public List<Person> findAllPersonsFollowingSessionToday() {
+        return sessionRepository.findAllPersonsFollowingSessionOnDate(LocalDate.now());
+    }
+
+    @Override
+    public List<Person> findAllPersonsFollowingSessionOnDate(LocalDate date) {
+        return sessionRepository.findAllPersonsFollowingSessionOnDate(date);
+    }
+
+    @Override
+    public List<Session> findSessionsToday() {
+        return sessionRepository.findSessionsOnDate(LocalDate.now());
+    }
+
+    @Override
+    public List<Session> findSessionsOnDate(LocalDate date) {
+        return sessionRepository.findSessionsOnDate(date);
+    }
+
+    @Override
+    public List<Session> findSessionsByInstructor(Instructor instructor) {
+        return sessionRepository.findSessionsByInstructor(instructor);
+    }
+
+    @Override
+    public Session findSessionByID(int id) throws SessionNotFoundException {
+        return sessionRepository.findSessionByID(id);
+    }
+}
