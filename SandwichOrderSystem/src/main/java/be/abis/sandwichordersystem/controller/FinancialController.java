2736d8504e043afbb38afeb388b119491170609b
Jana Heitkemper <janaheitkemper@gmail.com>
2022-11-10 15:53:11 +0100

    finished financial controller methods

--- SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/controller/FinancialController.java
+++ SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/controller/FinancialController.java
@@ -2,6 +2,7 @@ package be.abis.sandwichordersystem.controller;
 
 import be.abis.sandwichordersystem.exception.OrderNotFoundException;
 import be.abis.sandwichordersystem.exception.SessionNotFoundException;
+import be.abis.sandwichordersystem.model.Sandwich;
 import be.abis.sandwichordersystem.model.Session;
 import be.abis.sandwichordersystem.service.FinancialService;
 import be.abis.sandwichordersystem.service.SessionService;
@@ -10,6 +11,10 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/prices")
@@ -32,4 +37,34 @@ public class FinancialController {
         Session s = sessionService.findSessionByID(id);
         return financialService.calculateTotalPriceForSession(s);
     }
+
+    @GetMapping("period")
+    public Map<Session, Double> getPricesPerSessionForPeriod(@RequestParam("start") @DateTimeFormat(pattern = "d-M-yyyy") LocalDate startDate,
+                                                             @RequestParam("end") @DateTimeFormat(pattern = "d-M-yyyy") LocalDate endDate) throws OrderNotFoundException {
+        return financialService.calculatePricesPerSessionForPeriod(startDate, endDate);
+    }
+
+    @GetMapping("query")
+    public Map<Session, Double> getPricesPerSessionOnDate(@RequestParam("date") @DateTimeFormat(pattern = "d-M-yyyy") LocalDate date) throws OrderNotFoundException {
+
+        return financialService.calculatePricesPerSessionOnDate(date);
+    }
+
+    @GetMapping("sandwich-ranking/all/period")
+    public Map<Sandwich, Integer> getPopularityForPeriod(@RequestParam("start") @DateTimeFormat(pattern = "d-M-yyyy") LocalDate startDate,
+                                                         @RequestParam("end") @DateTimeFormat(pattern = "d-M-yyyy") LocalDate endDate) throws OrderNotFoundException {
+        return financialService.getPopularityOfSandwichesByDates(startDate, endDate);
+    }
+
+    @GetMapping("sandwich-ranking/top-{num}/period")
+    public Map<Sandwich, Integer> getTopPopularityForPeriod(@PathVariable int num, @RequestParam("start") @DateTimeFormat(pattern = "d-M-yyyy") LocalDate startDate,
+                                                         @RequestParam("end") @DateTimeFormat(pattern = "d-M-yyyy") LocalDate endDate) throws OrderNotFoundException {
+        return financialService.getPopularityOfSandwichesByDates(startDate, endDate).entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(num)
+                .collect(Collectors.toMap(
+                Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+    }
+
+
 }
--- SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/service/AbisFinancialService.java
+++ SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/service/AbisFinancialService.java
@@ -10,9 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -86,7 +84,11 @@ public class AbisFinancialService implements FinancialService {
             else popularity.put(s, 1);
         }
 
-        return popularity;
+        // return sorted map
+        return popularity.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 
     // getter and setter
