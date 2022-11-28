40a1a06b1456548ed84cad3b77479b04dc31e0ca
Marcel van Hassel <marcelvanhassel@gmail.com>
2022-11-28 12:30:16 +0100

    Mapped persons and sessions

--- SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/model/Student.java
+++ SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/model/Student.java
@@ -2,9 +2,15 @@ package be.abis.sandwichordersystem.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.persistence.*;
+
+@Entity
+@DiscriminatorValue("s")
 public class Student extends Person {
 
     @JsonIgnore
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "P_SID")
     private Session currentSession;
 
     public Student() {
