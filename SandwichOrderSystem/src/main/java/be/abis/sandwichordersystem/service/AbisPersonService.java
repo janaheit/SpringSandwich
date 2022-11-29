1230b051af1bb212b18a366e3a23b5650440f4e1
Marcel van Hassel <marcelvanhassel@gmail.com>
2022-11-29 15:57:55 +0100

    PersonService and PersonServiceTest implemented, all green!

--- SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/service/AbisPersonService.java
+++ SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/service/AbisPersonService.java
@@ -1,10 +1,12 @@
 package be.abis.sandwichordersystem.service;
 
+import be.abis.sandwichordersystem.exception.OperationNotAllowedException;
 import be.abis.sandwichordersystem.exception.PersonNotFoundException;
 import be.abis.sandwichordersystem.model.Admin;
 import be.abis.sandwichordersystem.model.Instructor;
 import be.abis.sandwichordersystem.model.Person;
 import be.abis.sandwichordersystem.model.Student;
+import be.abis.sandwichordersystem.repository.PersonJpaRepository;
 import be.abis.sandwichordersystem.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,49 +16,81 @@ import java.util.List;
 @Service
 public class AbisPersonService implements PersonService {
 
-    @Autowired private PersonRepository personRepository;
+    @Autowired private PersonJpaRepository personRepository;
 
     public AbisPersonService() {
     }
 
     @Override
     public Person findPersonByName(String name) throws PersonNotFoundException {
-        return personRepository.findPersonByName(name);
+        Person myPerson = personRepository.findPersonByName(name);
+        if (myPerson == null) {
+            throw new PersonNotFoundException("No person with name: " + name + " found");
+        }
+        return myPerson;
     }
 
     @Override
     public Instructor findInstructorByName(String name) throws PersonNotFoundException {
-        return personRepository.findInstructorByName(name);
+        Instructor myInstructor = personRepository.findInstructorByName(name);
+        if(myInstructor == null) {
+            throw new PersonNotFoundException("No instructor with name: " + name + " found");
+        }
+        return myInstructor;
     }
 
     @Override
     public Student findStudentByName(String name) throws PersonNotFoundException {
-        return personRepository.findStudentByName(name);
+        Student myStudent = personRepository.findStudentByName(name);
+        if(myStudent == null) {
+            throw new PersonNotFoundException("No student with name: " + name + " found");
+        }
+        return myStudent;
     }
 
     @Override
     public Admin findAdminByName(String name) throws PersonNotFoundException {
-        return personRepository.findAdminByName(name);
+        Admin myAdmin = personRepository.findAdminByName(name);
+        if(myAdmin==null) {
+            throw new PersonNotFoundException("No admin with name: " + name + " found");
+        }
+        return myAdmin;
     }
 
     @Override
     public Person findPersonByID(int id) throws PersonNotFoundException {
-        return personRepository.findPersonByID(id);
+        Person myPerson = personRepository.findPersonById(id);
+        if (myPerson==null) {
+            throw new PersonNotFoundException("No person found with id: " + id + "!");
+        }
+        return myPerson;
     }
 
     @Override
     public Instructor findInstructorByID(int id) throws PersonNotFoundException {
-        return personRepository.findInstructorByID(id);
+        Instructor myInstructor = personRepository.findInstructorByID(id);
+        if (myInstructor==null) {
+            throw new PersonNotFoundException("No instructor found with id: " + id + "!");
+        }
+        return myInstructor;
     }
 
     @Override
     public Student findStudentByID(int id) throws PersonNotFoundException {
-        return personRepository.findStudentByID(id);
+        Student myStudent = personRepository.findStudentByID(id);
+        if(myStudent==null) {
+            throw new PersonNotFoundException("No student found with id: " + id + "!");
+        }
+        return myStudent;
     }
 
     @Override
     public Admin findAdminByID(int id) throws PersonNotFoundException {
-        return personRepository.findAdminByID(id);
+        Admin myAdmin = personRepository.findAdminByID(id);
+        if(myAdmin==null) {
+            throw new PersonNotFoundException("No admin found with id: " + id + "!");
+        }
+        return myAdmin;
     }
 
     @Override
@@ -80,12 +114,24 @@ public class AbisPersonService implements PersonService {
     }
 
     @Override
-    public void addPerson(Person person) {
-        personRepository.addPerson(person);
+    public void addPerson(Person person) throws OperationNotAllowedException {
+        Person checkPerson = personRepository.findPersonByName(person.getFirstName()+person.getFirstName());
+        if (checkPerson!=null) {
+            throw new OperationNotAllowedException("Person already exists and cannot be added.");
+        }
+        if (person instanceof Student || person instanceof Admin || person instanceof Instructor) {
+            personRepository.save(person);
+        } else {
+            throw new OperationNotAllowedException("This person is not a Student, Instructor or Admin and cannot be added.");
+        }
     }
 
     @Override
     public void deletePerson(Person person) throws PersonNotFoundException {
-        personRepository.deletePerson(person);
+        Person checkPerson = personRepository.findPersonByName(person.getFirstName()+person.getLastName());
+        if (checkPerson==null) {
+            throw new PersonNotFoundException("No person found with name " + person.getFirstName() + " " + person.getLastName());
+        }
+        personRepository.deleteById(checkPerson.getPersonNr());
     }
 }
