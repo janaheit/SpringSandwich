a838f59406fe76417fbe1bf7309921085d59f260
Marcel van Hassel <marcelvanhassel@gmail.com>
2022-11-30 20:06:41 +0100

    Order object mapped, and OrderJpaRepository implemented and tested

--- SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/model/Order.java
+++ SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/model/Order.java
@@ -7,41 +7,73 @@ import be.abis.sandwichordersystem.enums.Options;
 import be.abis.sandwichordersystem.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
+@Entity
+@Table(name="orders")
 public class Order implements Comparable<Order> {
 
-    // Attributes
-    private static int orderCounter=0;
+    @SequenceGenerator(name="orderGen", sequenceName = "orders_oid_seq", allocationSize = 1)
 
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orderGen")
+    @Column(name="oid")
     private int orderNum;
+    @Column(name="ostatus")
+    @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus = OrderStatus.UNFILLED;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "o_pid")
     private Person person;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "o_sandid")
     private Sandwich sandwich;
+    @Column(name="obread")
+    @Enumerated(EnumType.STRING)
     private BreadType breadType;
+    @Column(name="oremark")
     private String remark;
+
+    @ElementCollection
+    @CollectionTable(name = "ORDEROPTIONS", joinColumns = @JoinColumn(name="OO_OID"))
+    @Column(name="option")
     private List<Options> options;
     //@JsonIgnore
     // You actually want to retrieve the session data in the api, just not all the students, but at least the name of the session
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name="o_sid")
     private Session session;
+    /* Deleted for now and added the seperate fields from the database
     @JsonIgnore
     private DayOrder dayOrder;
+     */
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "O_SHOP")
+    private SandwichShop sandwichShop;
+    @Column(name = "ODATE")
+    private LocalDate date;
+
     @JsonIgnore
+    @Column(name = "OPRICE")
     private double price;
+
     // TODO implement  amount in functions
+    @Column(name="OAMOUNT")
     private int amount;
 
     // Constructors
     public Order() {
-        this.orderNum = orderCounter;
-        orderCounter += 1;
     }
 
     public Order(Person person, DayOrder dayOrder) {
         this();
         this.person = person;
-        this.dayOrder = dayOrder;
+        this.date = dayOrder.getDate();
+        this.sandwichShop = dayOrder.getCurrentSandwichShop();
     }
 
     // Getters and setters
@@ -126,15 +158,32 @@ public class Order implements Comparable<Order> {
     }
 
     public DayOrder getDayOrder() {
-        return dayOrder;
+        return new DayOrder(this.sandwichShop, this.date);
     }
 
     public void setDayOrder(DayOrder dayOrder) {
-        this.dayOrder = dayOrder;
+        this.date = dayOrder.getDate();
+        this.sandwichShop = dayOrder.getCurrentSandwichShop();
     }
 
     public int getOrderNum() {
         return orderNum;
+    }
+
+    public SandwichShop getSandwichShop() {
+        return sandwichShop;
+    }
+
+    public void setSandwichShop(SandwichShop sandwichShop) {
+        this.sandwichShop = sandwichShop;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 
     // Compares
@@ -150,11 +199,11 @@ public class Order implements Comparable<Order> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Order order = (Order) o;
-        return orderNum == order.orderNum && person.equals(order.person) && Objects.equals(sandwich, order.sandwich) && Objects.equals(dayOrder, order.dayOrder);
+        return Double.compare(order.price, price) == 0 && amount == order.amount && orderStatus == order.orderStatus && Objects.equals(person, order.person) && Objects.equals(sandwich, order.sandwich) && breadType == order.breadType && Objects.equals(remark, order.remark) && Objects.equals(options, order.options) && Objects.equals(session, order.session) && Objects.equals(sandwichShop, order.sandwichShop) && Objects.equals(date, order.date);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(orderNum, person, sandwich, dayOrder);
+        return Objects.hash(orderStatus, person, sandwich, breadType, remark, options, session, sandwichShop, date, price, amount);
     }
 }
---
+++ SandwichOrderSystem/src/main/java/be/abis/sandwichordersystem/repository/OrderJpaRepository.java
@@ -0,0 +1,39 @@
+package be.abis.sandwichordersystem.repository;
+
+import be.abis.sandwichordersystem.enums.OrderStatus;
+import be.abis.sandwichordersystem.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface OrderJpaRepository extends JpaRepository<Order, Integer> {
+    /* for now we can use the standard save methods?
+    public boolean addOrder(Order order);
+    public boolean deleteOrder(Order order) throws OrderNotFoundException;
+     */
+
+    @Query(value = "select * from orders", nativeQuery = true)
+    public List<Order> getOrders();
+
+    @Query(value = "select * from orders where odate = :date", nativeQuery = true)
+    public List<Order> findOrdersByDate(@Param("date") LocalDate date);
+
+    @Query(value = "select * from orders where o_sid = :sId", nativeQuery = true)
+    public List<Order> findOrdersBySession(@Param("sId") int sessionId);
+
+    @Query(value = "select * from orders where odate >= :startDate and odate <= :endDate", nativeQuery = true)
+    public List<Order> findOrdersByDates(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    @Query(value = "select * from orders where odate >= :startDate and odate <= :endDate and ostatus = :status", nativeQuery = true)
+    public List<Order> findOrdersByStatusAndDates(@Param("status") String status, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    @Query(value = "select * from orders where ostatus = :status and o_sid = :sid", nativeQuery = true)
+    List<Order> findOrdersByStatusAndSession(@Param("status") String status, @Param("sid") int sessionId);
+
+    @Query(value = "select * from orders where odate >= :sdate and odate <= :edate and o_pid = :pid", nativeQuery = true)
+    public List<Order> findOrdersByPersonAndDates(@Param("pid") int personId,@Param("sdate") LocalDate startDate,@Param("edate") LocalDate endDate);
+
+}
